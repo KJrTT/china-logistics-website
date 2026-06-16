63d3016d928fb678061bf57fe28b4a4c014c5eba
kirill <kirillqwerttt@gmail.com>
2026-06-16 22:11:12 +0300

    Добавление 2-х страниц: контакты и преимущества. Реализовано оформление и стилизация внешнего вида для данных страниц. (В style.css). Был добавлен адаптив для корректного отображения на всех устройствах + для CTA-разметки. Также был реализован скрипт для плавного скрола после нажатия кнопки Заказать консультацию на странице Преимущества.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,16 @@
 // Общая логика сайта, то есть меню, адаптив, анимании какие-то и другие
+
+// Плавный скрол к якорю
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.hash) {
+        const target = document.querySelector(window.location.hash);
+        if (target) {
+            setTimeout(function() {
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }, 300);
+        }
+    }
+});
